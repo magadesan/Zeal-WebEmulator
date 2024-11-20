0e8b18b9accb6b0c884acce544d43750c2523cc2
ElamVaiyapuri <elamv@ElamVaipurisMBP.attlocal.net>
2024-11-20 11:59:29 -0600

    Remove Electron

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    on: (event, callback) => ipcRenderer.on(event, (_event, data) => callback(data)),
-    loaded: () => ipcRenderer.send("load", true)
-});
